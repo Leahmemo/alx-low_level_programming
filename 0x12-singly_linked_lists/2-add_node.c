2fc95d35ed64dab6a89cd636f6e132a2e99cf2bd
Leahmemo <glorialiah@gmail.com>
2023-06-02 11:20:49 -0700

    2-add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,9 +12,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	 unsigned int len = 0;
+	unsigned int len = 0;
 
-	 while (str[len])
+	while (str[len])
 		  len++;
 
 	if (str == NULL)
